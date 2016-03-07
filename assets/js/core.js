d1ddef79d07158a0c866c84f390a7debe63ca93a
TimboKZ <tima_kuzh@inbox.ru>
2016-03-07 21:32:37 +0000

    Finished working on number input matrix, moved on to steps and operations

--- assets/js/core.js
+++ assets/js/core.js
@@ -12,7 +12,10 @@ var GaussianElimination;
             this.workspace.html('');
             var that = this;
             var startMatrix = new StartMatrix(this.workspace, function (a, b, c) {
-                var inputMatrix = new InputMatrix(that.workspace, that.start, a, b, c);
+                that.workspace.html('');
+                var inputMatrix = new InputMatrix(that.workspace, function (numbers, divider) {
+                    that.start.call(that, numbers, divider);
+                }, a, b, c);
                 inputMatrix.render();
                 inputMatrix.getInput();
             });
@@ -20,28 +23,60 @@ var GaussianElimination;
             startMatrix.getInput();
         };
         Core.prototype.start = function (numbers, divider) {
+            console.log(this);
             this.workspace.html('');
-            this.initialMatrix = new Matrix();
+            this.initialMatrix = new Matrix(this.workspace, numbers, divider);
+            this.initialMatrix.render();
+            this.nextStep();
+        };
+        Core.prototype.nextStep = function () {
+            var operation = new Operation();
         };
         return Core;
     }());
     GaussianElimination.Core = Core;
-    var Step = (function () {
-        function Step() {
-        }
-        return Step;
-    }());
-    var Matrix = (function () {
-        function Matrix() {
-        }
-        return Matrix;
-    }());
-    GaussianElimination.Matrix = Matrix;
     var Operation = (function () {
         function Operation() {
         }
         return Operation;
     }());
+    var Step = (function () {
+        function Step() {
+        }
+        return Step;
+    }());
+    GaussianElimination.Step = Step;
+    var Matrix = (function () {
+        function Matrix(target, numbers, divider) {
+            this.target = target;
+            this.numbers = numbers;
+            this.divider = divider;
+            this.createNode();
+        }
+        Matrix.prototype.createNode = function () {
+            this.node = HTML.matrixBlock();
+            for (var i = 0; i < this.numbers.length; i++) {
+                var tr = HTML.tr();
+                var numberCounter = 0;
+                for (var k = 0; k < this.numbers[0].length + 1; k++) {
+                    if (k == this.divider) {
+                        tr.append(HTML.divider());
+                    }
+                    else {
+                        var td = HTML.td('' + this.numbers[i][numberCounter]);
+                        tr.append(td);
+                        numberCounter++;
+                    }
+                }
+                this.node.find('tbody').append(tr);
+            }
+        };
+        Matrix.prototype.render = function () {
+            this.target.append(this.node);
+        };
+        return Matrix;
+    }());
+    GaussianElimination.Matrix = Matrix;
     var InputMatrix = (function () {
         function InputMatrix(target, callback, a, b, c) {
             this.target = target;
@@ -53,13 +88,90 @@ var GaussianElimination;
             this.c = c;
         }
         InputMatrix.prototype.render = function () {
-            var block = HTML.matrixBlock();
+            var block = HTML.matrixBlock('mini-inputs');
             for (var i = 0; i < this.a; i++) {
-                for (var k = 0;;) {
+                var tr = HTML.tr();
+                this.inputs[i] = [];
+                var inputCounter = 0;
+                for (var k = 0; k < this.b + (this.c > 0 ? this.c + 1 : 0); k++) {
+                    if (k == this.b) {
+                        tr.append(HTML.divider());
+                    }
+                    else {
+                        var input = HTML.numberInput((i + 1) + ',' + (inputCounter + 1));
+                        this.inputs[i][inputCounter] = input;
+                        tr.append(HTML.td().append(input));
+                        inputCounter++;
+                    }
+                }
+                block.find('tbody').append(tr);
+            }
+            this.target.append(block);
+        };
+        InputMatrix.prototype.getInput = function () {
+            var that = this;
+            this.inputs[0][0].focus();
+            for (var i = 0; i < this.a; i++) {
+                for (var k = 0; k < this.b + this.c; k++) {
+                    console.log(i + ', ' + k);
+                    if (i == that.a - 1 && k == that.b + that.c - 1) {
+                        this.inputs[i][k].keyup(function (e) {
+                            if (e.which == 13) {
+                                if (that.validateInput()) {
+                                    that.fetchNumbers();
+                                }
+                            }
+                        });
+                    }
+                    else {
+                        (function () {
+                            var row = i, column = k;
+                            if (k == that.b + that.c - 1) {
+                                row++;
+                                column = 0;
+                            }
+                            else {
+                                column++;
+                            }
+                            that.inputs[i][k].keyup(function (e) {
+                                if (e.which == 13) {
+                                    that.inputs[row][column].focus();
+                                }
+                            });
+                        })();
+                    }
                 }
             }
         };
-        InputMatrix.prototype.getInput = function () {
+        InputMatrix.prototype.validateInput = function () {
+            var pass = true;
+            $('.error-input').removeClass('error-input');
+            for (var i = 0; i < this.a; i++) {
+                for (var k = 0; k < this.b + this.c; k++) {
+                    if (!this.isNumber(this.inputs[i][k].val())) {
+                        this.inputs[i][k].addClass('error-input');
+                        pass = false;
+                    }
+                }
+            }
+            if (pass)
+                this.errorManager.clear();
+            else
+                this.errorManager.error('Fields highlighted in red do not contain valid integers');
+            return pass;
+        };
+        InputMatrix.prototype.isNumber = function (number) {
+            return !isNaN(parseInt(number));
+        };
+        InputMatrix.prototype.fetchNumbers = function () {
+            var numbers = [];
+            for (var i = 0; i < this.a; i++) {
+                numbers[i] = [];
+                for (var k = 0; k < this.b + this.c; k++) {
+                    numbers[i][k] = parseInt(this.inputs[i][k].val());
+                }
+            }
+            this.callback(numbers, this.b);
         };
         return InputMatrix;
     }());
@@ -91,18 +203,24 @@ var GaussianElimination;
             });
         };
         StartMatrix.prototype.validateInput = function () {
-            if (!this.isNumber(this.a.val()) || parseInt(this.a.val()) < 1) {
-                this.errorManager.error('A should be an integer greater than zero!');
+            if (!this.isNumber(this.a.val()) || parseInt(this.a.val()) < 2) {
+                this.errorManager.error('A should be an integer greater than one!');
+                this.a.addClass('error-input');
                 return false;
             }
-            if (!this.isNumber(this.b.val()) || parseInt(this.b.val()) < 1) {
-                this.errorManager.error('B should be an integer greater than zero!');
+            if (!this.isNumber(this.b.val()) || parseInt(this.b.val()) < 2) {
+                this.errorManager.error('B should be an integer greater than one!');
+                this.b.addClass('error-input');
                 return false;
             }
             if (!this.isNumber(this.c.val()) || parseInt(this.c.val()) < 0) {
                 this.errorManager.error('C should be an integer greater than or equal to zero!');
+                this.c.addClass('error-input');
                 return false;
             }
+            this.a.removeClass('error-input');
+            this.b.removeClass('error-input');
+            this.c.removeClass('error-input');
             this.errorManager.clear();
             return true;
         };
@@ -156,8 +274,9 @@ var GaussianElimination;
     var HTML = (function () {
         function HTML() {
         }
-        HTML.matrixBlock = function () {
-            return $('<div class="block matrix"> <table><tbody></tbody></table></div>');
+        HTML.matrixBlock = function (_class) {
+            if (_class === void 0) { _class = ''; }
+            return $('<div class="block matrix ' + _class + '"> <table><tbody></tbody></table></div>');
         };
         HTML.tr = function () {
             return $('<tr></tr>');
--- src/ts/core.ts
+++ src/ts/core.ts
@@ -8,6 +8,7 @@ module GaussianElimination {
 
         private workspace:JQuery;
         private initialMatrix:Matrix;
+        private steps:Step[];
 
         constructor(workspace:JQuery) {
             this.workspace = workspace;
@@ -17,7 +18,10 @@ module GaussianElimination {
             this.workspace.html('');
             var that = this;
             var startMatrix = new StartMatrix(this.workspace, function (a:number, b:number, c:number) {
-                var inputMatrix = new InputMatrix(that.workspace, that.start, a, b, c);
+                that.workspace.html('');
+                var inputMatrix = new InputMatrix(that.workspace, function(numbers:number[][], divider:number){
+                    that.start.call(that, numbers, divider);
+                }, a, b, c);
                 inputMatrix.render();
                 inputMatrix.getInput();
             });
@@ -26,21 +30,64 @@ module GaussianElimination {
         }
 
         public start(numbers:number[][], divider:number) {
+            console.log(this);
             this.workspace.html('');
-            this.initialMatrix = new Matrix();
+            this.initialMatrix = new Matrix(this.workspace, numbers, divider);
+            this.initialMatrix.render();
+            this.nextStep();
+        }
+
+        public nextStep() {
+            var operation = new Operation();
         }
 
     }
 
-    class Step {
+    class Operation {
+
+
+
+    }
+
+    export class Step {
 
     }
 
     export class Matrix {
 
-    }
+        private target:JQuery;
+        private numbers:number[][];
+        private divider:number;
+        private node:JQuery;
 
-    class Operation {
+        constructor(target:JQuery,numbers:number[][], divider:number) {
+            this.target = target;
+            this.numbers = numbers;
+            this.divider = divider;
+            this.createNode();
+        }
+
+        private createNode() {
+            this.node = HTML.matrixBlock();
+            for (var i = 0; i < this.numbers.length; i++) {
+                var tr = HTML.tr();
+                var numberCounter = 0;
+                for (var k = 0; k < this.numbers[0].length + 1; k++) {
+                    if (k == this.divider) {
+                        tr.append(HTML.divider());
+                    } else {
+                        var td = HTML.td('' + this.numbers[i][numberCounter]);
+                        tr.append(td);
+                        numberCounter++;
+                    }
+                }
+                this.node.find('tbody').append(tr);
+            }
+        }
+
+        public render() {
+            this.target.append(this.node);
+        }
 
     }
 
@@ -65,16 +112,89 @@ module GaussianElimination {
         }
 
         public render() {
-            var block = HTML.matrixBlock();
-            for(var i = 0; i < this.a; i++) {
-                for(var k = 0;;) {
+            var block = HTML.matrixBlock('mini-inputs');
+            for (var i = 0; i < this.a; i++) {
+                var tr = HTML.tr();
+                this.inputs[i] = [];
+                var inputCounter = 0;
+                for (var k = 0; k < this.b + (this.c > 0 ? this.c + 1 : 0); k++) {
+                    if (k == this.b) {
+                        tr.append(HTML.divider());
+                    } else {
+                        var input = HTML.numberInput((i + 1) + ',' + (inputCounter + 1));
+                        this.inputs[i][inputCounter] = input;
+                        tr.append(HTML.td().append(input));
+                        inputCounter++;
+                    }
+                }
+                block.find('tbody').append(tr);
+            }
+            this.target.append(block);
+        }
 
+        public getInput() {
+            var that = this;
+            this.inputs[0][0].focus();
+            for (var i = 0; i < this.a; i++) {
+                for (var k = 0; k < this.b + this.c; k++) {
+                    console.log(i + ', ' + k);
+                    if (i == that.a - 1 && k == that.b + that.c - 1) {
+                        this.inputs[i][k].keyup(function (e) {
+                            if (e.which == 13) {
+                                if (that.validateInput()) {
+                                    that.fetchNumbers();
+                                }
+                            }
+                        });
+                    } else {
+                        (function () {
+                            var row = i, column = k;
+                            if (k == that.b + that.c - 1) {
+                                row++;
+                                column = 0;
+                            } else {
+                                column++;
+                            }
+                            that.inputs[i][k].keyup(function (e) {
+                                if (e.which == 13) {
+                                    that.inputs[row][column].focus();
+                                }
+                            });
+                        })();
+                    }
                 }
             }
         }
 
-        public getInput() {
+        private validateInput():boolean {
+            var pass = true;
+            $('.error-input').removeClass('error-input');
+            for (var i = 0; i < this.a; i++) {
+                for (var k = 0; k < this.b + this.c; k++) {
+                    if (!this.isNumber(this.inputs[i][k].val())) {
+                        this.inputs[i][k].addClass('error-input');
+                        pass = false;
+                    }
+                }
+            }
+            if (pass) this.errorManager.clear();
+            else this.errorManager.error('Fields highlighted in red do not contain valid integers');
+            return pass;
+        }
 
+        private isNumber(number:string):boolean {
+            return !isNaN(parseInt(number));
+        }
+
+        private fetchNumbers() {
+            var numbers = [];
+            for (var i = 0; i < this.a; i++) {
+                numbers[i] = [];
+                for (var k = 0; k < this.b + this.c; k++) {
+                    numbers[i][k] = parseInt(this.inputs[i][k].val());
+                }
+            }
+            this.callback(numbers, this.b);
         }
 
     }
@@ -116,18 +236,24 @@ module GaussianElimination {
         }
 
         private validateInput():boolean {
-            if (!this.isNumber(this.a.val()) || parseInt(this.a.val()) < 1) {
-                this.errorManager.error('A should be an integer greater than zero!');
+            if (!this.isNumber(this.a.val()) || parseInt(this.a.val()) < 2) {
+                this.errorManager.error('A should be an integer greater than one!');
+                this.a.addClass('error-input');
                 return false;
             }
-            if (!this.isNumber(this.b.val()) || parseInt(this.b.val()) < 1) {
-                this.errorManager.error('B should be an integer greater than zero!');
+            if (!this.isNumber(this.b.val()) || parseInt(this.b.val()) < 2) {
+                this.errorManager.error('B should be an integer greater than one!');
+                this.b.addClass('error-input');
                 return false;
             }
             if (!this.isNumber(this.c.val()) || parseInt(this.c.val()) < 0) {
                 this.errorManager.error('C should be an integer greater than or equal to zero!');
+                this.c.addClass('error-input');
                 return false;
             }
+            this.a.removeClass('error-input');
+            this.b.removeClass('error-input');
+            this.c.removeClass('error-input');
             this.errorManager.clear();
             return true;
         }
@@ -187,8 +313,8 @@ module GaussianElimination {
 
     export class HTML {
 
-        public static matrixBlock():JQuery {
-            return $('<div class="block matrix"> <table><tbody></tbody></table></div>');
+        public static matrixBlock(_class:string = ''):JQuery {
+            return $('<div class="block matrix ' + _class + '"> <table><tbody></tbody></table></div>');
         }
 
         public static tr():JQuery {
