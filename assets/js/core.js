a7ca32e94e692a76ae19aebcd8622ff7ca486fdb
TimboKZ <tima_kuzh@inbox.ru>
2016-03-07 19:19:14 +0000

    Introduced typescript classes, improved sass

--- assets/js/core.js
+++ assets/js/core.js
@@ -2,11 +2,39 @@
  * Gaussian elimination visualiser (https://github.com/TimboKZ/JS-Gaussian-Elimination)
  * Copyright (c) 2016 Timur Kuzhagaliyev (TimboKZ)
  */
-var GaussianElemination;
-(function (GaussianElemination) {
+var GaussianElimination;
+(function (GaussianElimination) {
     var Core = (function () {
-        function Core() {
+        function Core(workspace) {
+            this.workspace = workspace;
         }
+        Core.prototype.start = function () {
+            //this.workspace.html('');
+        };
         return Core;
     }());
-})(GaussianElemination || (GaussianElemination = {}));
+    GaussianElimination.Core = Core;
+    var Step = (function () {
+        function Step() {
+        }
+        return Step;
+    }());
+    var Operation = (function () {
+        function Operation() {
+        }
+        return Operation;
+    }());
+    var MatrixFactory = (function () {
+        function MatrixFactory() {
+        }
+        return MatrixFactory;
+    }());
+    GaussianElimination.MatrixFactory = MatrixFactory;
+    var Matrix = (function () {
+        function Matrix() {
+        }
+        return Matrix;
+    }());
+})(GaussianElimination || (GaussianElimination = {}));
+var core = new GaussianElimination.Core($('#workspace'));
+core.start();
--- src/ts/core.ts
+++ src/ts/core.ts
@@ -2,10 +2,42 @@
  * Gaussian elimination visualiser (https://github.com/TimboKZ/JS-Gaussian-Elimination)
  * Copyright (c) 2016 Timur Kuzhagaliyev (TimboKZ)
  */
-module GaussianElemination {
+module GaussianElimination {
 
-    class Core {
+    export class Core {
+
+        private workspace:JQuery;
+
+        constructor(workspace:JQuery) {
+            this.workspace = workspace;
+        }
+
+        public start() {
+            //this.workspace.html('');
+
+        }
+
+    }
+
+    class Step {
+
+    }
+
+    class Operation {
+
+    }
+
+    export class MatrixFactory {
+
+
+
+    }
+
+    class Matrix {
 
     }
 
 }
+
+var core = new GaussianElimination.Core($('#workspace'));
+core.start();
