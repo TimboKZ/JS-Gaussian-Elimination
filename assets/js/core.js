c7ad22e8b4117b45a5fa6eb6b93b5c65824fe3ba
TimboKZ <tima_kuzh@inbox.ru>
2016-03-07 18:23:11 +0000

    Setup the project and gulpfile

---
+++ assets/js/core.js
@@ -0,0 +1,12 @@
+/**
+ * Gaussian elimination visualiser (https://github.com/TimboKZ/JS-Gaussian-Elimination)
+ * Copyright (c) 2016 Timur Kuzhagaliyev (TimboKZ)
+ */
+var GaussianElemination;
+(function (GaussianElemination) {
+    var Core = (function () {
+        function Core() {
+        }
+        return Core;
+    }());
+})(GaussianElemination || (GaussianElemination = {}));
---
+++ src/ts/core.ts
@@ -0,0 +1,11 @@
+/**
+ * Gaussian elimination visualiser (https://github.com/TimboKZ/JS-Gaussian-Elimination)
+ * Copyright (c) 2016 Timur Kuzhagaliyev (TimboKZ)
+ */
+module GaussianElemination {
+
+    class Core {
+
+    }
+
+}
